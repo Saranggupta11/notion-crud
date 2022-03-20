848b20f522c4fce4a8e9532e5dd82bd8782a5176
saranggupta11 <sarang.gupta0811@gmail.com>
2022-03-21 01:47:50 +0530

    adding data to notion db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,46 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const { Client } = require("@notionhq/client");
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
+
+export default async function handler(req, res) {
+  const response = await notion.pages.create({
+    parent: {
+      database_id: process.env.NOTION_DB,
+    },
+    properties: {
+      title: {
+        title: [
+          {
+            text: {
+              content: "next js check",
+            },
+          },
+        ],
+      },
+      Description: {
+        rich_text: [
+          {
+            text: {
+              content: "next js desc",
+            },
+          },
+        ],
+      },
+      Project: {
+        checkbox: true,
+      },
+      Votes: {
+        number: 69,
+      },
+    },
+  });
+  console.log(response);
+  if (response) {
+    res.status(200).json(response);
+  } else {
+    res.status(500).json({ success: false });
+  }
 }
